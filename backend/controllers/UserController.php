9be9be7acd99588f92489c4090bd8d29ef3a551a
Sara894 <t48250klo@gmail.com>
2024-08-04 17:11:37 +0500

    создание репки

---
+++ backend/controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace backend\controllers;
+
+use common\models\LoginForm;
+use Yii;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\web\Response;
+use yii\rest\ActiveController;
+/**
+ * User controller
+ */
+class UserController extends ActiveController
+{
+ 
+}
